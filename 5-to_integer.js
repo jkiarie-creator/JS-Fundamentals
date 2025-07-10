63de37213ae1931c0300b2a9a2d6beaf463b4a98
jkiarie-creator <josmwangi101@gmail.com>
2025-07-10 12:59:14 +0300

    Adjust script output

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,5 +2,5 @@ const argv = process.argv;
 if (isNaN(argv[2]) || argv[2] === undefined) {
     console.log('Not a number');
 }else{
-    console.log('My number: ', parseInt(argv[2], 10));
+    console.log('My number:', parseInt(argv[2], 10));
 }
