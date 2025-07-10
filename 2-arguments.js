86298b94fec2f7343fc0ce8eb9b35c4bcf01a260
jkiarie-creator <josmwangi101@gmail.com>
2025-07-10 11:50:45 +0300

    Adjust output of the script

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,6 +1,6 @@
 const argv = process.argv;
 if(argv.length <= 2) {
-    console.log("No argument found");
+    console.log("No argument");
 }else if(argv.length === 3) {
     console.log("Argument found")
 }else{
