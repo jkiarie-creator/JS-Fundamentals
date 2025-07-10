96180f14abbb6ee880ceaa26a4ac581dfe7984aa
jkiarie-creator <josmwangi101@gmail.com>
2025-07-10 14:17:12 +0300

    Adjust script output

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,7 +1,7 @@
 let n = Number.parseInt(process.argv[2], 10);
 function factorial(n){
     if(Number.isNaN(n) || n < 0){
-        return 'Factorial of a negative number is undefined';
+        return 1;
     }
     else if (n === 0 || n === 1 ) {
         return 1;
